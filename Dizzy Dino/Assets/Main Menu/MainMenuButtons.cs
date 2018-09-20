0f6ac46812a212c862388e570cc93b3183344dc2
Mars Xue <marsxue0221@gmail.com>
2018-09-20 15:10:57 +1000

    edit main menu

---
+++ Dizzy Dino/Assets/Main Menu/MainMenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Play button
+	public void Play() {
+		SceneManager.LoadScene(1);
+	}
+
+	// Continue button
+	public void Continue() {
+
+	}
+	
+	// Quit button
+	public void Quit() {
+		Application.Quit();
+	}
+}
