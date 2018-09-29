e1dfd5a77cf9ecd5ffeb6a9096816927cae653a7
Eana Hufwe <ilove@1a23.com>
2018-09-29 20:57:00 +1000

    Moving dino across 3 lanes

---
+++ Dizzy Dino/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float laneWidth = 3f;
+    private float z = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        z = gameObject.transform.localPosition.z;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 pos = gameObject.transform.localPosition;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            pos.z = Mathf.Max(z - laneWidth, pos.z - laneWidth);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            pos.z = Mathf.Min(z + laneWidth, pos.z + laneWidth);
+        }
+        gameObject.transform.localPosition = pos;
+
+    }
+}
