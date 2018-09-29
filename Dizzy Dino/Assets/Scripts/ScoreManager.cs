8f63d7ed7ff617cd8f72d820ff21e6c9e4ea8679
Eana Hufwe <ilove@1a23.com>
2018-09-29 22:38:23 +1000

    Add placeholder score and score manager.

---
+++ Dizzy Dino/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static float score = 0f;
+    public Text highScore;
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score += Time.fixedDeltaTime;
+        int s = (int) (score * 5);
+        text.text = s.ToString().PadLeft(5, '0');
+        highScore.text = "HI " + s.ToString().PadLeft(5, '0');
+	}
+}
