5efaa839ee652ad637579afe52019f9ec52e13a4
Mars Xue <marsxue0221@gmail.com>
2018-10-03 11:42:46 +1000

    load game scene from menu

---
+++ Dizzy Dino/Assets/Menu/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void Update() {
+
+		if (Input.GetKey(KeyCode.Space)) {
+
+			Debug.Log("Did start the game!");
+
+			// game scene
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+		
+	}
+
+	public void Quit() {
+
+		Debug.Log("Did quit the game!");
+
+		Application.Quit();
+	}
+
+}
---
+++ Dizzy Dino/Assets/Menu/SettingMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingMenuScript : MonoBehaviour {
+
+	GameObject mainMenu;
+	GameObject settingMenu;
+
+	void Back() {
+
+		if (Input.GetKey(KeyCode.Space)) {
+			Debug.Log("Space");
+
+			// mainMenu.SetActive(true);
+			// settingMenu.SetActive(false);
+		
+		}
+
+	}
+
+}
