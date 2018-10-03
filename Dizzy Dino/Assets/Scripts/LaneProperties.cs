636954f42f280281e4422dd28eb81faf8fe53b81
Eana Hufwe <ilove@1a23.com>
2018-10-03 14:42:09 +1000

    Land obstacles showcase

---
+++ Dizzy Dino/Assets/Scripts/LaneProperties.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneProperties : MonoBehaviour {
+
+    public float speed; 
+	public float laneWidth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
