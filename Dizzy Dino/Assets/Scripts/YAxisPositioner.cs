9314ab23cc27f6f80e5f95e987be3be4ab79af38
Mars Xue <marsxue0221@gmail.com>
2018-10-08 15:47:18 +1100

    add fixed y position for all obstacles

---
+++ Dizzy Dino/Assets/Scripts/YAxisPositioner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YAxisPositioner : MonoBehaviour {
+
+	public float FixedY;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 p = gameObject.transform.position;
+
+		p.y = FixedY;
+
+		gameObject.transform.position = p;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
